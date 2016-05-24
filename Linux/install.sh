9a1931420e1084ce198300c6b1007d87b7fa1c2b
Jerry Liao <jerryliao26@gmail.com>
2016-05-24 08:05:26 +0800

    modify Linux version

--- Linux/install.sh
+++ Linux/install.sh
@@ -2,7 +2,7 @@
 echo "**This script must be run under root or the installation will not run successfully!"
 echo "**This program relys on Python3 and requests lib to run.Make sure you install them correctly."
 echo "**The Python3 directory is set as \"/usr/bin/python3\" by default."
-echo "**If it does not matches your Python3 path,you should edit the path yourself before installation."
+echo "**If it does not matches your Python3 path,you might need to edit the path yourself before installation."
 
 cp ./drcom.py /usr/bin/drcom
 mkdir -p ~/.drcom/
