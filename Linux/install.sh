f7b48af9348ed6546a9bda70d24ca43d866e91f4
Jerry Liao <jerryliao26@gmail.com>
2016-05-24 19:39:48 +0800

    correct some mistake

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "**This script must be run under root or the installation will not run successfully!"
-echo "**This program relys on Python and requests lib to run. Make sure you install them correctly."
+echo "**This program relies on Python and requests lib to run. Make sure you install them correctly."
 echo "**The Python directory is set as \"/usr/bin/env python\" by default."
 echo "**If it does not matches your Python path,you might need to edit the path yourself before installation."
 
