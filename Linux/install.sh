fae0cc3927e58072277ca1e5a98356d500d88703
Jerry Liao <jerryliao26@gmail.com>
2016-05-24 19:38:54 +0800

    correct some mistake

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "**This script must be run under root or the installation will not run successfully!"
-echo "**This program relys on Python and requests lib(not urllib version) to run.Make sure you install them correctly."
+echo "**This program relys on Python and requests lib to run. Make sure you install them correctly."
 echo "**The Python directory is set as \"/usr/bin/env python\" by default."
-echo "**If it does not matches your Python3 path,you might need to edit the path yourself before installation."
+echo "**If it does not matches your Python path,you might need to edit the path yourself before installation."
 
 cp ./drcom.py /usr/bin/drcom
 mkdir -p ~/.drcom/
--- Linux/uninstall.sh
+++ Linux/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "**This script must be run under root or the uninstallation will not run successfully!"
-echo "**The uninstallation will only delete files created by default settings.If you have change the settings,you will need to delete it MANUALLY."
+echo "**The uninstallation will only delete files created by default settings. If you have change the settings,you will need to delete it MANUALLY."
 
 rm -f /usr/bin/drcom
 
@@ -12,7 +12,7 @@ then
   exit 0
 
 else
-  echo "**Uninstallation finish!You can delete user config file by reruning this script!"
+  echo "**Uninstallation finish!You can delete user config file by rerunning this script!"
   exit 0
 
 fi
