7f53df267187733945607465621e0804c3760eb8
Jerry Liao <jerryliao26@gmail.com>
2016-05-24 08:06:36 +0800

    fix paths in install.sh

--- Linux/install.sh
+++ Linux/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "**This script must be run under root or the installation will not run successfully!"
 echo "**This program relys on Python3 and requests lib to run.Make sure you install them correctly."
-echo "**The Python3 directory is set as \"/usr/bin/python3\" by default."
+echo "**The Python3 directory is set as \"/usr/bin/env python3\" by default."
 echo "**If it does not matches your Python3 path,you might need to edit the path yourself before installation."
 
 cp ./drcom.py /usr/bin/drcom
