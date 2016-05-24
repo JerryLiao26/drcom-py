f591539887a06527efa7d0727c11fc55dbc32353
Jerry Liao <jerryliao26@gmail.com>
2016-05-24 23:23:17 +0800

    add Embedded version

---
+++ Embedded/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "**This script must be run under root or the installation will not run successfully!"
+echo "**This program relies on Python to run. Make sure you install them correctly."
+echo "**The Python directory is set as \"/usr/bin/env python\" by default."
+echo "**If it does not matches your Python path,you might need to edit the path yourself before installation."
+
+cp ./drcom.py /usr/bin/drcom
+mkdir -p ~/.drcom/
+chmod 777 ~/.drcom/
+
+echo "**Installation finish!"
